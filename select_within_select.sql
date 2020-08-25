3a2c9b9369c4aaaf7cc0268c84f779b544cf6911
Anup Samuel <57674855+anupsam7@users.noreply.github.com>
2020-08-25 19:11:11 -0400

    Add files via upload

--- select_within_select.sql
+++ select_within_select.sql
@@ -73,4 +73,47 @@ WHERE gdp > (
 		WHERE continent = 'Europe'
 		)
 
--- 
+-- Find the largest country (by area) in each continent, show the continent, the name and the area
+SELECT continent
+	,name
+	,area
+FROM world x
+WHERE area >= ALL (
+		SELECT area
+		FROM world y
+		WHERE x.continent = y.continent
+		)
+
+-- List each continent and the name of the country that comes first alphabetically.
+SELECT continent
+	,name
+FROM world x
+WHERE name <= ALL (
+		SELECT name
+		FROM world y
+		WHERE x.continent = y.continent
+		)
+
+-- Find the continents where all countries have a population <= 25000000.
+-- Then find the names of the countries associated with these continents. Show name, continent and population.
+SELECT name
+	,continent
+	,population
+FROM world x
+WHERE 25000000 >= ALL (
+		SELECT population
+		FROM world y
+		WHERE x.continent = y.continent
+		)
+
+-- Some countries have populations more than three times that of any of their neighbours (in the same continent).
+-- Give the countries and continents.
+SELECT name
+	,continent
+FROM world x
+WHERE population / 3 > ALL (
+		SELECT population
+		FROM world y
+		WHERE x.continent = y.continent
+			AND x.name != y.name
+		)
